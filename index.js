c0cd70433aac30e24f930cad9d99c39cc7c49c64
ShahFH <shahfaisal.swe@gmail.com>
2023-07-18 12:59:40 +0430

    Connected to database

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
 const app = express();
 
-// import Routers
+// Connection URL for MongoDB
+MongoClient.connect("mongodb+srv://ShahLead:3e6e6u0V64qmbzyK@cluster0.4vb8zor.mongodb.net/test?retryWrites=true&w=majority", () => console.log("Connected to MongoDB"))
+
+
+// Import Routers
 const authRoute = require('./routes/auth');
 
-
 // Route Middlewares
-app.use('./api/user', authRoute);
+app.use('/api/user', authRoute);
 
-app.listen(300, () => console.log("Server Up and running"))
+app.listen(3000, () => {
+  console.log("Server Up and running on port 3000");
+});
